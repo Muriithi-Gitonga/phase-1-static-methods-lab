e05e5f312fd2bbb0bc309562f555171beee171cd
Muriithi-Gitonga <rinkanyadavid8@gmail.com>
2022-09-13 12:54:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
   //add static methods here
+  static capitalize (string) {
+    return string.replace(string[0], string[0].toUpperCase());
+  }
+
+  static sanitize (string) {
+    return string.replace(/[^A-Za-z0-9-'\s]+/g, "");
+  }
+
+  static titleize (string) {
+    
+    // let arr = [];
+    let exludeWords = ["a", "an", "but", "of", "and", "for", "at", "by","from", "the"]
+    let arr = string.split(' ');
+    return arr.map((word, i) =>{ 
+     return exludeWords.includes(word) && i!=0 ? [word] : word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(" ")
+
+  }
 }
+
+Formatter.titleize("A Tale of Two Cities")
